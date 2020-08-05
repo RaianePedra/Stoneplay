2fbfa31263bf6bb279edfa3a4ab4e1ffa5c3e799
Raiane_Pedra <56899926+RaianePedra@users.noreply.github.com>
2020-08-05 00:53:04 -0300

    Update index.js

--- src/components/Menu/components/ButtonLink/index.js
+++ src/components/Menu/components/ButtonLink/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 function ButtonLink(props) {
-<<<<<<< HEAD
   // props => { className: "o que alguém passar", href: "/" }
   return (
     <a href={props.href} className={props.className}>
@@ -11,15 +10,3 @@ function ButtonLink(props) {
 }
 
 export default ButtonLink;
-=======
-    //props => {className: "O que alguem passar", href:"/"}
-    console.log(props);
-    return (
-        <a href={props.href}  className={props.className}>
-            {props.children}
-        </a>
-    );
-}
-
-export default ButtonLink;  
->>>>>>> 427b58d1f670444d0f77ce60471c26bcc29206cb
