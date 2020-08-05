6fafa0c68bf8661b340329819d58854ddb93d48f
Raiane_Pedra <56899926+RaianePedra@users.noreply.github.com>
2020-08-05 00:52:30 -0300

    Update index.js

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import LOGO from '../../assets/img/LOGO.png';
 import './Menu.css';
-<<<<<<< HEAD
 import Button from '../Button';
 // import ButtonLink from './components/ButtonLink';
 
@@ -15,28 +14,9 @@ function Menu() {
 
       <Button as={Link} className="ButtonLink" to="/cadastro/video">
         Novo vídeo
-=======
-import Button from '../Button'
-
-//import ButtonLink from './components/ButtonLink';
-
-function Menu() {
-  return (
-    <nav className ='Menu'>
-      <Link to= '/'>
-        <img className = 'Logo' src = {LOGO} alt = 'Stoneflix logo'/>   
-      </Link>
-            
-      <Button as= {Link} className='ButtonLink' to='/cadastro/video'>
-        Novo Video
->>>>>>> 427b58d1f670444d0f77ce60471c26bcc29206cb
       </Button>
     </nav>
   );
 }
 
-<<<<<<< HEAD
 export default Menu;
-=======
-export default Menu;  
->>>>>>> 427b58d1f670444d0f77ce60471c26bcc29206cb
