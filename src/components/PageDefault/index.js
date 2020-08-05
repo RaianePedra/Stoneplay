2fb7a113cf1d7d9d0490036c251c570072e4b5de
Raiane_Pedra <56899926+RaianePedra@users.noreply.github.com>
2020-08-05 00:53:29 -0300

    Update index.js

--- src/components/PageDefault/index.js
+++ src/components/PageDefault/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
-<<<<<<< HEAD
 import styled, { css } from 'styled-components';
 
 import Menu from '../Menu';
 import Footer from '../Footer';
-=======
 import Menu from '../Menu';
 import Footer from '../Footer';
 import styled from 'styled-components';
->>>>>>> 427b58d1f670444d0f77ce60471c26bcc29206cb
 
 const Main = styled.main`
   background-color: var(--black);
@@ -17,7 +14,6 @@ const Main = styled.main`
   padding-top: 50px;
   padding-left: 5%;
   padding-right: 5%;
-<<<<<<< HEAD
   ${({ paddingAll }) => css`
     padding: ${paddingAll};
   `}
@@ -29,16 +25,6 @@ function PageDefault({ children, paddingAll }) {
     <>
       <Menu />
         <Main paddingAll={paddingAll}>
-=======
-`;
-
-
-function PageDefault({ children }) {
-  return (
-    <>
-      <Menu />
-        <Main>
->>>>>>> 427b58d1f670444d0f77ce60471c26bcc29206cb
           {children}
         </Main>
       <Footer />
