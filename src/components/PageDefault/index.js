c42c32b6f675d04f0a16dbc59574eb5d25559b2d
Raiane_Pedra <56899926+RaianePedra@users.noreply.github.com>
2020-08-05 00:56:08 -0300

    Update index.js

--- src/components/PageDefault/index.js
+++ src/components/PageDefault/index.js
@@ -3,8 +3,6 @@ import styled, { css } from 'styled-components';
 
 import Menu from '../Menu';
 import Footer from '../Footer';
-import Menu from '../Menu';
-import Footer from '../Footer';
 import styled from 'styled-components';
 
 const Main = styled.main`
